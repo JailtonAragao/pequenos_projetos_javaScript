89e2f2ea86c8f25fde91d991b08b7e0c9b1700c1
jailton aragao <jailton.aragao@gmail.com>
2022-08-14 14:09:05 -0300

    Farorado o código e testes de array desestruturado. Fim do projeto_01

--- projeto_01.js
+++ projeto_01.js
@@ -3,8 +3,11 @@
     inteiros como argumentos e retorne a média aritmética entre eles. 
 */
 
+
+
 //Função
 
+/*
 function media(numbers) {
 
     let soma = 0;
@@ -20,10 +23,47 @@ function media(numbers) {
     return media;
 
 }
+*/
 
+// Fatorando com reduce
 
-//Teste da função.
+/*
+function media(numbers) {
+    const soma = numbers.reduce((acumulado, numero) => { // reduce recebe uma função de callback vindo de todos os elementos do array e vai acumulando
+                                    // reduce(parametros) => {}
+        return acumulado + numero; // retorna o valor acumulado
+        
+    }, 0) // ) tem a função de vaslor inicial ao acumulado
+
+    return soma / numbers.length; // retorna para media
+}
+*/
+
+//Abreviando o codigo
+
+/*
+function media(numbers) {
+    const soma = numbers.reduce((acumulado, numero) => acumulado + numero, 0)
+    return soma / numbers.length; // retorna para media
+}
+*/
+
+//Desestruturando o Array, quando somente numeros é passado.
+
+function media(...numbers) { // (...) indica desestruturação do array 
+    const soma = numbers.reduce((acumulado, numero) => acumulado + numero, 0)
+    return soma / numbers.length; // retorna para media
+}
+
+//Teste da função com numeros ... em uma chamada desestruturada.
+
+console.log(media(10, 9, 6, 8, 9, 1, 5, 7)) //6.875
+console.log(media(2, 5, 7, 1, -2)) // 2.6
+
+/*
+//Teste da função com Array.
 console.log(media([10, 9, 6, 8, 9, 1, 5, 7])) //6.875
 console.log(media([2, 5, 7, 1, -2])) // 2.6
 console.log(media([10, 10, 10, 10, 9])) // 9.8
 console.log(media([25, 75])) //50
+*/
