c3696e443763d033491650680744f600ce418253
jailton aragao <jailton.aragao@gmail.com>
2022-08-14 14:19:25 -0300

    Iniciando o projeto_02

--- projeto_01.js
+++ projeto_01.js
@@ -1,4 +1,13 @@
 /*
+Trabalhar com uma quantidade qualquer de números, sejam 3 ou 3000. 
+Para isso podemos usar arrays, assim ao chamar a função temos a 
+liberdade de passar quantos números quisermos e isso será tratado 
+corretamente dentro dela.
+*/
+
+//Projeto_01
+
+/*
     Escreva uma função que recebe um número qualquer de números
     inteiros como argumentos e retorne a média aritmética entre eles. 
 */
@@ -48,20 +57,22 @@ function media(numbers) {
 }
 */
 
-//Desestruturando o Array, quando somente numeros é passado.
+//Desestruturando o Array, utilizar operador spread para que os 
+//parâmetros não precisem ser passados como um array:
 
 function media(...numbers) { // (...) indica desestruturação do array 
     const soma = numbers.reduce((acumulado, numero) => acumulado + numero, 0)
     return soma / numbers.length; // retorna para media
 }
 
-//Teste da função com numeros ... em uma chamada desestruturada.
+//Teste da função com numeros (...) em uma chamada desestruturada.
 
 console.log(media(10, 9, 6, 8, 9, 1, 5, 7)) //6.875
 console.log(media(2, 5, 7, 1, -2)) // 2.6
 
 /*
 //Teste da função com Array.
+
 console.log(media([10, 9, 6, 8, 9, 1, 5, 7])) //6.875
 console.log(media([2, 5, 7, 1, -2])) // 2.6
 console.log(media([10, 10, 10, 10, 9])) // 9.8
---
+++ projeto_02.js
@@ -0,0 +1,9 @@
+/*
+
+Recursão é quando uma função chama ela mesma até chegar em um caso base 
+simples que desencadeia a resolução do problema. 
+Recursão é um conceito muito importante e comum na computação, 
+ela permite que problemas grandes e complexos sejam quebrados 
+em tarefas mais simples e claras.
+
+*/
